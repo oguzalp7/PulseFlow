4dcef7875c840eb4ea8de12330a8eec2939c838c
Oguz Alp Saglam <oguzalp7@github.com>
2025-03-27 21:22:10 +0300

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,7 @@ import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
   dest: 'public',
-  output: "standalone",
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,   // Check for changes every second
-      aggregateTimeout: 300,   // Delay before rebuilding
-    };
-    return config;
-  },
+  
 });
 
 export default nextConfig;
